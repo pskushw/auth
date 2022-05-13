b9381e44f1dd0b6f7c385ce64a85e5e3988bc441
nidhibsp.singh@gmail.com <prku@softwareag.com>
2022-05-13 11:36:54 +0530

    connected to mongo db and created schema/model

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+
+dotenv.config();
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT, 
+    () => console.log("connected to db"));
 
 //Import Routes
-const authRoute = require('./routes/auth');
+const authRoute = require("./routes/auth");
 
 //Route Middlewares
-app.use('/api/user', authRoute);
+app.use("/api/user", authRoute);
 
-app.listen(3005,()=>console.log('server up and running'));
+app.listen(3005, () => console.log("server up and running"));
