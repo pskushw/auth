7b7343f0427880aef621a12c2a816564933e8f09
nidhibsp.singh@gmail.com <prku@softwareag.com>
2022-05-21 15:22:21 +0530

    connected to mongodb

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
-dotenv.config();
-//connect to DB
-mongoose.connect(process.env.DB_CONNECT, 
-    () => console.log("connected to db"));
 
 //Import Routes
 const authRoute = require("./routes/auth");
 
+dotenv.config();
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT, () => console.log("connected to db"));
+
+//Middleware
+app.use(express.json());
+
 //Route Middlewares
 app.use("/api/user", authRoute);
+app.get("/", (req, res) => {
+  res.send(req.header("User-Agent"));
+});
 
 app.listen(3005, () => console.log("server up and running"));
