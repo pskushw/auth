7800482e2cdab37d162b1e0c878a05852975c44b
nidhibsp.singh@gmail.com <prku@softwareag.com>
2022-06-06 16:07:35 +0530

    validation for existing emails

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,18 @@ router.post('/register', async (req, res) => {
     //Let validate the data before we make a user
     const {error} = validateRegister(req.body);
     if(error) return res.status(400).send(error.details[0].message);
+    
+    // Checking if the user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email});
+    if(emailExist) return res.status(400).send("Email already exists");
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     });
+    
     try {
         const savedUser = await user.save();
         res.json(savedUser);
